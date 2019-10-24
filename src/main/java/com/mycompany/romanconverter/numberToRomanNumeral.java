78114680439e7e35a6083549f82159182dc47deb
Craig Bell <craigdanielbell@gmail.com>
2019-10-24 13:43:58 +0100

    Amended numberToRomanNumeral for invalid input range
    Output a message on the webpage instead of the console and captured it in an if statement. We could also add further validation if they try to enter in a string instead of numbers.

--- src/main/java/com/mycompany/romanconverter/numberToRomanNumeral.java
+++ src/main/java/com/mycompany/romanconverter/numberToRomanNumeral.java
@@ -33,82 +33,86 @@ public class numberToRomanNumeral {
         String roman="";
          
          
-        while(number<=0 || number>3999){
+        if(number<=0 || number>3999){
             System.out.println("Invalid input.  You must enter a number between 1 and 3999");
-            System.out.print("Please enter another number now: ");
-            number = scan.nextInt();
-        }
-         
-        while(number>=1000){
-            roman += "M";
-            number-=1000;
-        }
-         
-        while(number>=900){
-            roman += "CM";
-            number-=900;
-        }
-         
-        while(number>=500){
-            roman += "D";
-            number-=500;
-        }
-         
-        while(number>=400){
-            roman += "CD";
-            number-=400;
-        }
-         
-        while(number>=100){
-            roman += "C";
-            number-=100;
-        }
-         
-        while(number>=90){
-            roman += "XC";
-            number-=90;
-        }
-         
-        while(number>=50){
-            roman += "L";
-            number-=50;
-        }
-         
-        while(number>=40){
-            roman += "XL";
-            number-=40;
-        }
-         
-        while(number>=10){
-            roman += "X";
-            number-=10;
-        }
-         
-        while(number>=9){
-            roman += "IX";
-            number-=9;
-        }
-         
-        while(number>=5){
-            roman += "V";
-            number-=5;
-        }
-         
-        while(number>=4){
-            roman += "IV";
-            number-=4;
-        }
-         
-        while(number>=1){
-            roman += "I";
-            number-=1;
-        }
-         
-        System.out.println("The Roman Numeral is "+ roman);
-        
-        
-        String output = "<h1>Conversion</h1>Your roman numeral is: <strong> " + roman + "</strong>!";
-        return Response.status(200).entity(output).build();
+            System.out.println("Please enter another number now: ");
+            
+            String output = "<h1>Oh no!!</h1><p style=\"font-size: 22px\">Invalid input.  You must enter a number between 1 and 3999. Please enter another number now!</p>";
+            return Response.status(200).entity(output).build();
+            
+            //number = scan.nextInt();
+        }else{
+            while(number>=1000){
+                roman += "M";
+                number-=1000;
+            }
+
+            while(number>=900){
+                roman += "CM";
+                number-=900;
+            }
+
+            while(number>=500){
+                roman += "D";
+                number-=500;
+            }
+
+            while(number>=400){
+                roman += "CD";
+                number-=400;
+            }
+
+            while(number>=100){
+                roman += "C";
+                number-=100;
+            }
+
+            while(number>=90){
+                roman += "XC";
+                number-=90;
+            }
+
+            while(number>=50){
+                roman += "L";
+                number-=50;
+            }
+
+            while(number>=40){
+                roman += "XL";
+                number-=40;
+            }
+
+            while(number>=10){
+                roman += "X";
+                number-=10;
+            }
+
+            while(number>=9){
+                roman += "IX";
+                number-=9;
+            }
+
+            while(number>=5){
+                roman += "V";
+                number-=5;
+            }
+
+            while(number>=4){
+                roman += "IV";
+                number-=4;
+            }
+
+            while(number>=1){
+                roman += "I";
+                number-=1;
+            }
+
+            System.out.println("The Roman Numeral is "+ roman);
+
+
+            String output = "<h1>Conversion</h1><p style=\"font-size: 22px\">Your roman numeral is: <strong> " + roman + "</strong>!</p>";
+            return Response.status(200).entity(output).build();
+        }//endofif
     }//sayRomanNumeral
 }//numberToRomanNumeral
 
