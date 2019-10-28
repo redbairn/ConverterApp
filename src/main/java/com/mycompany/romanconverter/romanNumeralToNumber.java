615a0709636ddb813071e80172a3a8e69677aa49
Craig Bell <craigdanielbell@gmail.com>
2019-10-28 12:19:21 +0000

    Amended the RomanNumeralToNumber java file
    Amended it and it works better but still buddy. Try out MM and MXMX!

--- src/main/java/com/mycompany/romanconverter/romanNumeralToNumber.java
+++ src/main/java/com/mycompany/romanconverter/romanNumeralToNumber.java
@@ -16,74 +16,89 @@ import javax.ws.rs.core.Response;
  * @author craig bell
  * 
  */
-
+// Example: http://127.0.0.1:49000/api/roman2num/MXMX Gives 2000 - need to amend.
 @Path("/roman2num")
 public class romanNumeralToNumber {
         static String romanNumeralToNumber;
         static int decimalNum;
         
+        
+        private int letterToNumber(char letter) {
+             // Find the integer value of letter considered as a Roman numeral.  Return
+             // -1 if letter is not a legal Roman numeral.  The letter must be upper case.
+          switch (letter) {
+             case 'I':  return 1;
+             case 'V':  return 5;
+             case 'X':  return 10;
+             case 'L':  return 50;
+             case 'C':  return 100;
+             case 'D':  return 500;
+             case 'M':  return 1000;
+             default:   return -1;
+          }
+       }
+        
+        
+        
         @GET
         @Path("/{param}")
         public Response sayNumeral(@PathParam("param") String romanNumeralToNumber) {
-                
             // Set the RomanNumeral to an uppercase value
-                romanNumeralToNumber = romanNumeralToNumber.toUpperCase();
+            romanNumeralToNumber = romanNumeralToNumber.toUpperCase();
+            
+            if (romanNumeralToNumber.length() == 0){
+             throw new NumberFormatException("An empty string does not define a Roman numeral.");
+            }
+           
                 
-                // Create a variable to be the length of the RomanNumeral
-                int len=romanNumeralToNumber.length();
-                int num=0; // Initialised num at zero to begin with
-                int previousnum = 0; // Initialised previousnum at zero to begin with
-                for (int i=len-1;i>=0;i--) // Loop through the characters in the string
-                { 
-                        // CharAt(i) returns the character at the specified index in the string "romanNumeralToNumber". 
-                        // The index value that we pass in this method should be between 0 and (length of string-1)
-                        // We set the value to be a character named 'x'.
-                        char x = romanNumeralToNumber.charAt(i);
-                        //x = Character.toUpperCase(x);
-                        // If the equality is found, switch starts to execute the code starting from the corresponding case, 
-                        // until the nearest break (or until the end of switch).
-                        switch(x)
-                        {  
-                            case 'I':
-                                previousnum = num;
-                                num = 1; // This is the value for this case
-                                break; 
-                            case 'V':
-                                previousnum = num;
-                                num = 5;
-                                break;
-                            case 'X':
-                                previousnum = num;
-                                num = 10;
-                                break;
-                            case 'L':
-                                previousnum = num;
-                                num = 50;
-                                break;
-                            case 'C':
-                                previousnum = num;
-                                num = 100;
-                                break;
-                            case 'D':
-                                previousnum = num;
-                                num = 500;
-                                break;
-                            case 'M':
-                                previousnum = num;
-                                num = 1000;
-                                break;
-                        }  
-                        // Possibly working correctly here with the break but not in the else
-                      if (num<previousnum){
-                            decimalNum = decimalNum-num;
-                            break; // For testing purposes
-                      }else{ 
-                          // Problems here becuase the value gets added to the previous amount
-                            decimalNum = decimalNum+num;
-                            break; // For testing purposes
-                      }
-                }//endOfFor
-                String output = "<h1>Conversion</h1><p style=\"font-size: 22px\">Your numeral is: <strong> " + decimalNum + "</strong>!</p>";
+            // Create a variable to be the length of the RomanNumeral
+            int len=romanNumeralToNumber.length();
+            int num=0; // Initialised num at zero to begin with
+
+            int i=0;
+            int arabic = 0;
+                        
+            while (i < len) {
+
+                char letter = romanNumeralToNumber.charAt(i);        // Letter at current position in string.
+                int number = letterToNumber(letter);  // Numerical equivalent of letter.
+
+                if (number < 0)
+                   throw new NumberFormatException("Illegal character \"" + letter + "\" in roman numeral.");
+
+                i++;  // Move on to next position in the string
+
+                if (i == romanNumeralToNumber.length()) {
+                      // There is no letter in the string following the one we have just processed.
+                      // So just add the number corresponding to the single letter to arabic.
+                   arabic += number;
+                }
+                else {
+                      // Look at the next letter in the string.  If it has a larger Roman numeral
+                      // equivalent than number, then the two letters are counted together as
+                      // a Roman numeral with value (nextNumber - number).
+                   int nextNumber = letterToNumber(romanNumeralToNumber.charAt(i));
+                   if (nextNumber > number) {
+                         // Combine the two letters to get one value, and move on to next position in string.
+                      arabic += (nextNumber - number);
+                      i++;
+                   }
+                   else {
+                         // Don't combine the letters.  Just add the value of the one letter onto the number.
+                      arabic += number;
+                   }
+                }
+
+            }  // end while
+            
+            if (arabic > 3999){
+                throw new NumberFormatException("Roman numeral must have value 3999 or less.");    
+            }
+            num = arabic;  
+                        
+                        
+                   
+                String output = "<h1>Conversion</h1><p style=\"font-size: 22px\">Your numeral is: <strong> " + arabic + "</strong>!</p>";
                 // Using the Response builder we output the HTML above with the correct value with a HTTP status code 200
                 return Response.status(200).entity(output).build();
         }//sayNumeral      
